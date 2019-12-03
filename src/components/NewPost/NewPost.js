44c20a2c80d463dcb69e524f078dabe8288edf27
anna_rgmv <annaragimov@gmail.com>
2019-12-04 01:10:27 +0200

    adding axios, creating post requests

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './NewPost.css';
 
@@ -6,8 +7,21 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Anna'
     }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            authos: this.state.author
+        };
+
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => {
+                console.log(response.data);
+            })
+    } 
 
     render () {
         return (
@@ -20,9 +34,9 @@ class NewPost extends Component {
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Manu">Anna</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,44 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        postSelectedId: null
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatePosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Anna'
+                }
+            })
+            this.setState({posts: updatePosts});
+        });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({postSelectedId: id})
+
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                        title={post.title}
+                        key={post.id}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.postSelectedId}/>
                 </section>
                 <section>
                     <NewPost />
